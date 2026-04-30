18b684aad85360afbbe34cf93427dd7aa62f81ce
mlight lee <mlight.lee@outlook.com>
2026-04-30 13:14:02 +0800

    feat: add typedoc comments for MLEADER and MLEADERSTYLE

--- src/parser/entities/leader/types.ts
+++ src/parser/entities/leader/types.ts
@@ -2,23 +2,120 @@ import type { Point3D } from '../../../types/shared.ts'
 import type { CommonDxfEntity } from '../shared.ts'
 import type { LeaderCreationFlag } from './consts.ts'
 
+/**
+ * LEADER entity fields mapped from AutoCAD DXF group codes.
+ *
+ * Reference:
+ * https://help.autodesk.com/cloudhelp/2024/ENU/AutoCAD-DXF/files/GUID-396B2369-F89F-47D7-8223-8B7FB794F9F3.htm
+ */
 export interface LeaderEntity extends CommonDxfEntity {
+  /**
+   * Group code 0: entity type.
+   * Expected value: `LEADER`.
+   */
   type: 'LEADER'
+
+  /**
+   * Group code 100: subclass marker for this entity.
+   * Expected value: `AcDbLeader`.
+   */
   subclassMarker: 'AcDbLeader'
+
+  /**
+   * Group code 3: dimension style name used by this leader.
+   */
   styleName: string
+
+  /**
+   * Group code 71: arrowhead flag.
+   * 0 = disabled, 1 = enabled.
+   */
   isArrowheadEnabled: boolean
+
+  /**
+   * Group code 72: leader path type.
+   * 0 = straight line segments, 1 = spline.
+   */
   isSpline: boolean
+
+  /**
+   * Group code 73: leader creation flag.
+   * 0 = text annotation, 1 = tolerance annotation,
+   * 2 = block reference annotation, 3 = no annotation.
+   */
   leaderCreationFlag: LeaderCreationFlag
+
+  /**
+   * Group code 74: hookline direction flag.
+   * 0 = hookline (or end tangent for spline leader) is opposite
+   * to the horizontal direction vector.
+   * 1 = hookline (or end tangent for spline leader) is same as
+   * the horizontal direction vector.
+   */
   isHooklineSameDirection: boolean
+
+  /**
+   * Group code 75: hookline flag.
+   * 0 = no hookline, 1 = has hookline.
+   */
   isHooklineExists: boolean
+
+  /**
+   * Group code 40: text annotation height.
+   */
   textHeight?: number
+
+  /**
+   * Group code 41: text annotation width.
+   */
   textWidth?: number
+
+  /**
+   * Group code 76: number of leader vertices.
+   * AutoCAD notes this value is ignored for OPEN operations.
+   */
   numberOfVertices?: number
+
+  /**
+   * Group codes 10/20/30 (repeated): leader vertex coordinates.
+   * 10 = X, 20 = Y, 30 = Z for each vertex.
+   */
   vertices: Point3D[]
+
+  /**
+   * Group code 77: color to use when leader's `DIMCLRD` is BYBLOCK.
+   */
   byBlockColor?: number
+
+  /**
+   * Group code 340: hard reference handle to associated annotation
+   * (for example MTEXT, TOLERANCE, or INSERT).
+   */
   associatedAnnotation?: string
+
+  /**
+   * Group codes 210/220/230: normal vector.
+   * 210 = X, 220 = Y, 230 = Z.
+   */
   normal?: Point3D
+
+  /**
+   * Group codes 211/221/231: horizontal direction vector for the leader.
+   * 211 = X, 221 = Y, 231 = Z.
+   */
   horizontalDirection?: Point3D
+
+  /**
+   * Group codes 212/222/232: offset of the last leader vertex
+   * from the block reference insertion point.
+   * 212 = X, 222 = Y, 232 = Z.
+   */
   offsetFromBlock?: Point3D
+
+  /**
+   * Group codes 213/223/233: offset of the last leader vertex
+   * from the annotation placement point.
+   * 213 = X, 223 = Y, 233 = Z.
+   */
   offsetFromAnnotation?: Point3D
 }
